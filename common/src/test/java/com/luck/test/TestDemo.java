eb06b0d5544683165165bef35ba69c7c1a751c98
luck <luck__chen@163.com>
2017-12-28 14:38:50 +0800

    add testdemo

---
+++ common/src/test/java/com/luck/test/TestDemo.java
@@ -0,0 +1,17 @@
+package com.luck.test;
+
+import com.luck.common.utils.StringUtil;
+import org.junit.Test;
+
+/**
+ * Created by luck on 2017/12/28.
+ */
+public class TestDemo {
+
+
+    @Test
+    public void testStr(){
+        String s = "";
+        System.out.println(StringUtil.isBlank(s));
+    }
+}
